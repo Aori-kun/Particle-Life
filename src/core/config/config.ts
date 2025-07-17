0f1a782a9db84b333896f0f8c9d03066b5c46b28
Aori-san <dupaswebstudio@gmail.com>
2025-07-17 23:03:34 +0200

    feat(conf): update conf (#26)
    * feat(conf): update conf
    
    - Change nb particles
    - Change velocity
    
    ref : #002
    
    * feat(conf): add flavicon
    
    ref : #002

--- src/core/config/config.ts
+++ src/core/config/config.ts
@@ -1,4 +1,4 @@
-export const NB_PARTICLES: number = 1000;
+export const NB_PARTICLES: number = 1250;
 export const NB_COLORS: number = 6;
 export const PARTICLE_SIZE: number = 2.5;
-export const VELOCITY: number = 0.7;
+export const VELOCITY: number = 0.9;
